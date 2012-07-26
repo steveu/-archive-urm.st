b6949c64fb0511abea0033948a0c2fe6aadbcb15
Steve Urmston <steve@urm.st>
2012-07-26 15:53:56 +0200

    Tinkering with transitions

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,29 +1,28 @@
 <?php snippet('header'); ?>
 
+<section class="home">
 
-<h1 class="title"><?=markdown($page->heading())?></h1>
+	<h1 class="title">
+		<strong>Hello there</strong>, I'm Steve, a freelance web designer & developer based in North Yorkshire. <em>I design and build interfaces for web &amp; mobile</em>.
+	</h1>
 
+	<?php $projects = $pages->findByTitle('Projects'); ?>
+	<?php $items = $projects->children()->visible()->flip(); ?>
+	<?php if($items && $items->count()): ?>
+		
+		<?php foreach($items AS $item): ?>
+			<article>
+				<a href="/projects/<?=$item->id()?>">
+					<figure>
+						<?php $image = $item->images()->find('screenshot.png') ?>
+						<img src="<?php echo $image->url() ?>" alt="<?=$item->title()?>" />
+					</figure>
+				</a>	
+			</article>
+		<?php endforeach; ?>
 
-<?php $projects = $pages->findByTitle('Projects'); ?>
-<?php $items = $projects->children()->visible()->flip(); ?>
-<?php if($items && $items->count()): ?>
-	
-	<section class="home_projects">
+	<?php endif; ?>
 
-	<?php foreach($items AS $item): ?>
-		<article>
-			<a href="/projects/<?=$item->id()?>">
-				<figure>
-					<?php $image = $item->images()->find('screenshot.png') ?>
-					<img src="<?php echo $image->url() ?>" alt="<?=$item->title()?>" />
-				</figure>
-			</a>	
-		</article>
-	<?php endforeach; ?>
-
-	</section>
-
-<?php endif; ?>
-
+</section>
 
 <?php snippet('footer'); ?>
